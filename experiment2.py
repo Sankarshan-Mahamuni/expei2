6659569df462dae8b47321705835136432528f6a
Sankarshan-Mahamuni <150650631+Sankarshan-Mahamuni@users.noreply.github.com>
2023-12-17 13:22:51 +0530

    Update experiment2.py

--- experiment2.py
+++ experiment2.py
@@ -2,65 +2,39 @@
 
 import streamlit as st
 import pandas as pd
-import math
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
 
 st.title('EXPERIMENT haha haha haha 2')
 st.header('Determine dissociation constant Ka of Weak acid (Acetic acid) using pH-meter.')
 st.header('Prior Concepts:')
-st.write('Concentration of H+ and OH-, pH, pOH, pKa, pKb. Concept of strong andweak acids and bases, Buffers: acidic and basic, dissociation constant (Ka)')
+st.write('Concentration of H+ and OH-, pH, pOH, pKa, pKb. Concept of strong and weak acids and bases, Buffers: acidic and basic, dissociation constant (Ka)')
 st.header('New Concept:')
 st.subheader('Proposition 1: pH metric Titration')
 st.write('Study of pH-metric titration of weak acid against strong base using pH-meter without use of any indicators.')
 
-# Option 1: Provide the path to the image file
-#image_path = r"C:\Users\Arpita\OneDrive\Pictures\Screenshots\Screenshot 2023-11-19 202426.png"
-#st.image(image_path,use_column_width=True)
-import streamlit as st
-from PIL import Image
-
-# Open an image file
-img_path = "exp2_1.png"
-img = Image.open(img_path)
-
-# Display the image using Streamlit
-st.image(img,  use_column_width=True)
+# Display image 1
+img_path_1 = "exp2_1.png"
+img_1 = Image.open(img_path_1)
+st.image(img_1, use_column_width=True)
 
 st.subheader('Proposition 2:')
 st.write('Nature of graph depends on the fact that')
 st.write('i) Acetic acid being weak acid feebly dissociates and hence up to the stage of complete neutralization pH of system increases very mildly due to formation of buffer CH3COONa + CH3COOH.')
 
-
-    # Option 1: Provide the path to the image file
-#image_path =r"C:\Users\Arpita\OneDrive\Pictures\Screenshots\Screenshot 2023-11-19 203238.png"
-#st.image(image_path, use_column_width=True)
-import streamlit as st
-from PIL import Image
-
-# Open an image file
-img_path = "exp2_2.png"
-img = Image.open(img_path)
-
-# Display the image using Streamlit
-st.image(img,  use_column_width=True)
+# Display image 2
+img_path_2 = "exp2_2.png"
+img_2 = Image.open(img_path_2)
+st.image(img_2, use_column_width=True)
 
 st.subheader('Proposition 3:')
 st.write('Determination of dissociation constant Ka of weak acid (acetic acid) using Henderson’s equation.')
 
-
-    # Option 1: Provide the path to the image file
-#image_path = r"C:\Users\Arpita\OneDrive\Pictures\Screenshots\Screenshot 2023-11-19 204820.png"
-#st.image(image_path, use_column_width=True)
-import streamlit as st
-from PIL import Image
-
-# Open an image file
-img_path = "exp2_3.png"
-img = Image.open(img_path)
-
-# Display the image using Streamlit
-st.image(img,  use_column_width=True)
+# Display image 3
+img_path_3 = "exp2_3.png"
+img_3 = Image.open(img_path_3)
+st.image(img_3, use_column_width=True)
 
 st.header('Learning Objective:')
 st.subheader('Intellectual skills:')
@@ -103,9 +77,8 @@ def main():
     ka_values = calculate_ka(observations["pH"])
 
     # Display calculated Ka values
-    # Display calculated Ka values
-    st.write("Calculated Ka values:", [f"{value:.4f}" for value in ka_values])
-
+    formatted_ka_values = [f"{value:.4f}" for value in ka_values]
+    st.write("Calculated Ka values:", formatted_ka_values)
 
     # Create a DataFrame from the observation table
     df_observations = pd.DataFrame(observations)
@@ -122,17 +95,6 @@ def main():
     st.pyplot(plt)
     st.write('pH = -log(Ka)')
 
-    # Additional code (commented out) for calculating Ka at the end point
-    # Uncomment and customize as needed
-    # end_point_index = st.number_input("Enter the observation index at the end point of titration:", min_value=1, step=1)
-    # pH_at_end_point = df_observations.at[end_point_index - 1, "pH"] if "pH" in df_observations.columns else None
-    # st.write('pH = -log(Ka)')
-    # st.header("Result:")
-    # st.write("1. End Point of Titration:", df_observations.at[end_point_index - 1, "Volume of NaOH added"], "ml")
-    # st.write("2. pH at End Point:", pH_at_end_point)
-    # if pH_at_end_point is not None:
-    #     dissociation_constant = calculate_ka(pH_at_end_point)
-    #     st.write("3. Dissociation Constant of Weak Acid (Ka):", dissociation_constant)
-
 if __name__ == "__main__":
     main()
+
